b89b51e32a32d7ab2f30ec280f4c2a772c571871
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-08-29 15:06:01 +0200

    Add Blog

---
+++ src/data/posts/firstpost.md
@@ -0,0 +1,10 @@
+---
+title: First post
+published: 2023-06-16
+updated: null
+description: Welcome to my blog!
+---
+
+Welcome to my blog!
+
+I will sometimes shared some more information about my Projects
---
+++ src/lib/components/Controls.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import CopyURL from './CopyURL.svelte';
+	import GoBack from './GoBack.svelte';
+	export let variant = 'top';
+</script>
+
+<div class={variant}>
+	<GoBack />
+	<CopyURL />
+</div>
+
+<style>
+	div {
+		display: flex;
+		justify-content: space-between;
+		color: var(--secondary-font-color);
+	}
+	div.bottom {
+		margin-block: 1rem 2rem;
+	}
+</style>
