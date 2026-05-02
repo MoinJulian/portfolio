a980536bc08cf66de3c267724878bae9f1abf22b
Julian Hammer <j.hammer@swingalytica.com>
2026-05-02 01:06:23 +0100

    feat: update PostCSS configuration to use tailwindcss and autoprefixer plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+import tailwindPostcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+	plugins: [
+		tailwindPostcss(),
+		autoprefixer(),
+	],
 };
